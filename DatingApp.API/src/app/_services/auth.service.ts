5c9ed2cfd7e732e800f6992842642d11440e4c37
Salil <salil.oak@hotmail.com>
2019-06-14 10:18:46 +0530

    Added Alertify and Auth0 services

--- DatingApp.API/src/app/_services/auth.service.ts
+++ DatingApp.API/src/app/_services/auth.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 //Service is not a component hence the Injectable attribute needs to be added
 @Injectable()
 export class AuthService {
 
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
 
   constructor(private http: HttpClient) { }
 
@@ -28,5 +30,10 @@ export class AuthService {
 
   register(model: any) {
     return this.http.post(this.baseUrl + 'register', model);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 }
