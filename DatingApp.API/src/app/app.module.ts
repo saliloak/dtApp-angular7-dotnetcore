7d7b83a4f955366b0564ae4918b32ae4ec838368
Salil <salil.oak@hotmail.com>
2019-06-28 11:38:08 +0530

    Added Member Edit component

--- DatingApp.API/src/app/app.module.ts
+++ DatingApp.API/src/app/app.module.ts
@@ -24,6 +24,8 @@ import { JwtModule } from '@auth0/angular-jwt';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { NgxGalleryModule } from 'ngx-gallery';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -40,7 +42,8 @@ export function tokenGetter() {
       ListsComponent,
       MessagesComponent,
       MemberCardComponent,
-      MemberDetailComponent
+      MemberDetailComponent,
+      MemberEditComponent
     ],
     imports: [
       BrowserModule,
@@ -65,7 +68,8 @@ export function tokenGetter() {
       AuthGuard,
       UserService,
       MemberDetailResolver,
-      MemberListResolver
+      MemberListResolver,
+      MemberEditResolver
     ],
     bootstrap: [AppComponent]
   })
